83c7ce14292df5f589bba0507205b807e53c8065
tsaievan <tsaievan@icloud.com>
2018-02-22 16:04:53 +0800

    纯代码写, 创建窗口并设置根控制器, 使自己可以看到黄色背景的view

---
+++ YFWeibo/YFWeibo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YFWeibo
+//
+//  Created by tsaievan on 22/2/18.
+//  Copyright © 2018年 tsaievan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.yellow
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
