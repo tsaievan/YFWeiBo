146cae98498a213c17cb602267f0065a3ad392c9
tsaievan <tsaievan@icloud.com>
2018-02-25 19:54:55 +0800

    加载webView

--- YFWeibo/YFWeibo/Classes/ViewControllers/OAuth/YFOAuthController.swift
+++ YFWeibo/YFWeibo/Classes/ViewControllers/OAuth/YFOAuthController.swift
@@ -8,16 +8,24 @@
 
 import UIKit
 
-class YFOAuthController: UIViewController {
+class YFOAuthController: UIViewController, UIWebViewDelegate {
+    lazy var webView: UIWebView = {
+        let wb = UIWebView(frame: SCREEN_BOUNDS)
+        wb.delegate = self
+        return wb
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        loadWebView()
     }
 }
 
 extension YFOAuthController {
     fileprivate func setupUI() {
-        view.backgroundColor = .yellow
+        view.backgroundColor = .white
+        view.addSubview(webView)
     }
 }
 
@@ -26,3 +34,22 @@ extension YFOAuthController {
         dismiss(animated: true, completion: nil)
     }
 }
+
+extension YFOAuthController {
+    fileprivate func loadWebView() {
+        let urlString = "https://api.weibo.com/oauth2/authorize?client_id=4029511368&redirect_uri=https://www.baidu.com"
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        webView.loadRequest(URLRequest(url: url))
+    }
+}
+
+// MARK: webView的代理
+extension YFOAuthController {
+    func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        return true
+    }
+}
+
+
